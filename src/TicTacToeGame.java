4aa24bce2fa2b4e866174fa674a39715acac0c3c
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-08 11:51:31 +0530

    Showing the board
    Added the function that shows the board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -18,10 +18,8 @@ public class TicTacToeGame {
 	}
 	public static void showBoard(char[][] board){
 		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				System.out.print(board[i][j]);
-			}
-			System.out.println();
+			System.out.println(board[i][0]+"|"+board[i][2]+"|"+board[i][2]);
+			System.out.println("_____");
 		}
 	}
 	public static void main(String []args) {
