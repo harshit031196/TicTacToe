d1402479c73580a17aac003fff538835c31203e4
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-08 09:28:12 +0530

    Created a board

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TicTacToeGame {
+	public static char[] useCase1(){
+		char[] board = {' ', ' ',' ',' ',' ',' ',' ',' ',' ', ' '};
+		return board;
+	}
+	public static void main(String []args) {
+		char[] newBoard = useCase1();
+	}
+}
