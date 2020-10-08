5412e70c6f0429721e5521ce059044dc159911d3
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-08 09:28:48 +0530

    Delete TicTacToeGame.java

--- TicTacToeGame.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class TicTacToeGame {
-	public static void useCase1(){
-		char[] board = {' ', ' ',' ',' ',' ',' ',' ',' ',' ', ' '};
-	}
-	public static void main(String []args) {
-		useCase1();
-	}
-}
