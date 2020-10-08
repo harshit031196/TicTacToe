a577839d5b12ece3a79c5add5ec04b8b1e31593c
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-08 10:15:53 +0530

    Take the player's option

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,10 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	public static char[] useCase1(){
+	public static char[] makeBoard(){
 		char[] board = {' ', ' ',' ',' ',' ',' ',' ',' ',' ', ' '};
 		return board;
 	}
+	public static char chooseOption() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please choose between X and O");
+		char c = sc.next().charAt(0);
+		return c;
+	}
 	public static void main(String []args) {
-		char[] newBoard = useCase1();
+		Scanner sc = new Scanner(System.in);
+		char[] newBoard = makeBoard();
+		char playerOption=chooseOption();
+		System.out.println("Please enter the input");
+		char input = sc.next().charAt(0);
+		if(input==playerOption) {
+			System.out.println("This is the player's option");
+		}else {
+			System.out.println("This is the computer's option");
+		}
 	}
 }
